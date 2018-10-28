da3132510d9c0420c9813eb6c39e349bf633e196
Steven Brehmer <software4steve@gmail.com>
2018-10-27 21:15:29 -0400

    Start model descriptions

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route   get api/posts/test
+// @desc    Tests post route
+// @access  Public route
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
 
 //Have to export the router for the server.js file to pick it up.
