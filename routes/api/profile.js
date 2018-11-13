de0aa919177eb19ebd70ab13c7d8eb0d6b4fac76
Steven Brehmer <software4steve@gmail.com>
2018-11-13 17:18:27 -0500

    Hotfix key exposed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,5 +34,46 @@ router.get(
   }
 );
 
+// @route   POST api/profile
+// @desc    Create user profile
+// @access  Private route
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // gather all fields that come in
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.status) profileFields.status = req.body.status;
+    // Split skills into array from CSV
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    if (req.body.skills) profileFields.skills = req.body.skills;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    if (req.body.experience.title)
+      profileFields.experience.title = req.body.experience.title;
+    if (req.body.experience.company)
+      profileFields.experience.company = req.body.experience.company;
+    if (req.body.experience.location)
+      profileFields.experience.location = req.body.experience.location;
+    if (req.body.experience.from)
+      profileFields.experience.from = req.body.experience.from;
+    if (req.body.experience.to)
+      profileFields.experience.to = req.body.experience.to;
+    if (req.body.experience.current)
+      profileFields.experience.current = req.body.experience.current;
+    if (req.body.experience.description)
+      profileFields.experience.description = req.body.experience.description;
+  }
+);
+
 //Have to export the router for the server.js file to pick it up.
 module.exports = router;
