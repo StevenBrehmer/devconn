73663e7c641e82fb8688382cf41ad45817d9e780
Steven Brehmer <software4steve@gmail.com>
2018-12-19 19:44:32 -0500

    Finished register front end.. woop!

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,17 @@
-import { TEST_DISPATCH } from "./types";
+import { GET_ERRORS } from "./types";
+import axios from "axios";
 
 // Register User
-export const registerUser = userData => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  };
+export const registerUser = (userData, history) => dispatch => {
+  // This should be inside an asyc call.
+  // thus we nee dto use thunk
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-//import axios from "axios";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 import classnames from "classnames";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
@@ -15,6 +15,20 @@ class Register extends Component {
       password2: "",
       errors: {}
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // Here we can test for new properties
+    // Test for Errors properties
+
+    // Get Errors from Redux State
+    // Put into props with MapStateToProps
+    // once we receive new properties.. if errors
+    // then set to component state ( errors in constructor)
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onChange = e => {
@@ -32,23 +46,16 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    /*axios
-      .post("/api/users/register", newUser)
-      .then(res => console.log(res.data))
-      .catch(err => this.setState({ errors: err.response.data }));
-    */
-    this.props.registerUser(newUser);
+    //use this.props.history to redirect from this action.
+    this.props.registerUser(newUser, this.props.history);
     console.log(newUser);
   };
 
   render() {
     const { errors } = this.state;
 
-    const { user } = this.props.auth;
-
     return (
       <div className="register">
-        {user ? user.name : null}
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -133,13 +140,15 @@ class Register extends Component {
 
 Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
 export default connect(
   mapStateToProps,
   { registerUser }
-)(Register);
+)(withRouter(Register));
